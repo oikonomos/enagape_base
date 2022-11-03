2722881b6d0b92e965601e3a3a6502e934af3570
Sangbom Suhk <imbeliever@naver.com>
2022-11-03 16:18:18 +0900

    Commit Test.

--- src/main/java/net/enagape/base/repository/CommentRepository.java
+++ src/main/java/net/enagape/base/repository/CommentRepository.java
@@ -5,5 +5,6 @@ import java.util.List;
 import net.enagape.base.entity.EaComment;
 import org.springframework.data.repository.CrudRepository;
 
-public interface CommentRepository extends CrudRepository<EaComment, Long> {	
+public interface CommentRepository extends CrudRepository<EaComment, Long> {
+	
 }
